c471d63b92676a618f41071130a74e88b1a5894e
veeranki2014 <veeranki2014@gmail.co>
2021-08-02 16:26:52 +0530

    Demo on EC2 Instance

---
+++ 04-remote-state/sample.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "sample" {
+  ami                     = "ami-074df373d6bafa625"
+  instance_type           = "t3.micro"
+   tags = {
+    Name                  = "sample"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-anka-devops"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
