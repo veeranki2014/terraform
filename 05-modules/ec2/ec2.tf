5e87cb0ab080733f3094ab6598273302fd5bb814
veeranki2014 <veeranki2014@gmail.com>
2021-08-14 15:09:54 +0530

    Demo on Modules

--- 05-modules/ec2/ec2.tf
+++ 05-modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   count                   = 2
   ami                     = "ami-074df373d6bafa625"
-  instance_type           = [var.TYPE]
+  instance_type           = var.TYPE
   vpc_security_group_ids  = [var.SG_ID]
 
   tags = {
@@ -12,9 +12,9 @@ resource "aws_instance" "sample" {
 variable "SG_ID" {}
 variable "TYPE" {}
 
-output "TYPE" {
-  value = aws_instance.sample.instance_type
-}
+//output "TYPE" {
+//  value = aws_instance.sample.instance_type
+//}
 
 //output "PRIVATE_IP" {
 //  value = "aws_instance.sample.private_ip"
