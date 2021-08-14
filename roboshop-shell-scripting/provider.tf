bf441b88a83308dbff03913bfe4ba5781a95b6b8
veeranki2014 <veeranki2014@gmail.com>
2021-08-14 21:02:40 +0530

    Demo on Modules

--- roboshop-shell-scripting/provider.tf
+++ roboshop-shell-scripting/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket                = "terraform-roboshop"
+    bucket                = "terraform-anka-devops"
     key                   = "roboshop/shell-scripting/terraform.tfstate"
     region                = "us-east-1"
     dynamodb_table        = "terraform"
