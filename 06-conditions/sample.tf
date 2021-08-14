75021f2a742a16352fb6b70f75d30289cce1c184
veeranki2014 <veeranki2014@gmail.com>
2021-08-14 17:00:26 +0530

    Demo on Modules

--- 06-conditions/sample.tf
+++ 06-conditions/sample.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  count                   = 0
+ # count                   = 0
   ami                     = "ami-074df373d6bafa625"
   instance_type           = var.TYPE == null ? "t3.small" : var.TYPE
   #vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
