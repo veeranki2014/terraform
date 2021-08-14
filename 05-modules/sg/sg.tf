103ccf77da6d4e6f8c05b0130a821e5909aa17be
veeranki2014 <veeranki2014@gmail.com>
2021-08-14 13:19:16 +0530

    Demo on Modules

---
+++ 05-modules/sg/sg.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow_ssh" {
+  name                = "allow_ssh"
+  description         = "allow_ssh"
+
+  ingress {
+    description       = "ssh"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1" #its allow all ports 0 to 65535
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name              = "allow_ssh"
+  }
+}
