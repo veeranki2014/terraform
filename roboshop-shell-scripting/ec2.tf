9b2859341c9f07787372a2ed8378468c3d88f19d
veeranki2014 <veeranki2014@gmail.com>
2021-08-15 15:40:06 +0530

    Demo on Modules

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -11,12 +11,12 @@ resource "aws_spot_instance_request" "cheap_worker" {
   }
 }
 
-//resource "aws_ec2_tag" "name-tag" {
-//  count                 = length(var.COMPONENTS)
-//  resource_id           = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-//  key                   = "Name"
-//  value                 = element(var.COMPONENTS,count.index)
-//}
+resource "aws_ec2_tag" "name-tag" {
+  count                 = length(var.COMPONENTS)
+  resource_id           = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key                   = "Name"
+  value                 = element(var.COMPONENTS,count.index)
+}
 
 output "out" {
   value = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
