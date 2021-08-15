618a40acf7241bd7c54d63792b8f2f9bb589af80
veeranki2014 <veeranki2014@gmail.com>
2021-08-15 19:25:48 +0530

    Demo on Modules

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                   = "ami-074df373d6bafa625"
   spot_price            = "0.0294"
   instance_type         = "t3.micro"
+  vpc_security_group_ids = ["sg-0bcba096b73fa353d"]
   wait_for_fulfillment  = true
 
   tags                  = {
@@ -21,3 +22,21 @@ resource "aws_ec2_tag" "name-tag" {
 //output "out" {
 //  value = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
 //}
+
+resource "null_resource" "run-shell-scripting" {
+  provisioner "remote-exec" {
+    connection {
+      count             = length(var.COMPONENTS)
+      host              = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
+      user              = "centos"
+      password          = "DevOps321"
+    }
+    inline             = [
+      "cd /home/centos",
+      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+      "cd shell-scripting/roboshop",
+      "sudo make ${element(var.COMPONENTS,count.index)}"
+      ]
+    }
+}
+
