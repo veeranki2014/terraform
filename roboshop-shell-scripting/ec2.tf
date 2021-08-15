a7ba22cd2fbd079e2d6a5b92f5ec0f782462ab70
veeranki2014 <veeranki2014@gmail.com>
2021-08-15 20:20:32 +0530

    Demo on Modules

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -28,23 +28,43 @@ resource "aws_route53_record" "records" {
   records                 = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
 }
 
+//resource "null_resource" "run-shell-scripting" {
+//  depends_on              = [aws_route53_record.records]
+//  provisioner "remote-exec" {
+//    connection {
+//      count               = local.LENGTH
+//      host                = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
+//      user                = "centos"
+//      password            = "DevOps321"
+//    }
+//    inline                = [
+//      "cd /home/centos",
+//      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+//      "cd shell-scripting/roboshop",
+//      "git pull",
+//      "sudo make ${element(var.COMPONENTS, count.index)}"
+//      ]
+//    }
+//}
+
 resource "null_resource" "run-shell-scripting" {
-  depends_on              = [aws_route53_record.records]
+  depends_on                = [aws_route53_record.records]
+  count                     = local.LENGTH
   provisioner "remote-exec" {
     connection {
-      count               = local.LENGTH
-      host                = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
-      user                = "centos"
-      password            = "DevOps321"
+      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
+      user                  = "centos"
+      password              = "DevOps321"
     }
-    inline                = [
+
+    inline = [
       "cd /home/centos",
       "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
       "cd shell-scripting/roboshop",
       "git pull",
       "sudo make ${element(var.COMPONENTS, count.index)}"
-      ]
-    }
+    ]
+  }
 }
 
 locals {
