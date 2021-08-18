c80702cc6464f09d5b6b452d570aadd9809b62bf
veeranki2014 <veeranki2014@gmail.com>
2021-08-18 18:15:42 +0530

    Demo on Roboshop Project

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_spot_instance_request" "cheap_worker" {
   count                   = local.LENGTH
   ami                     = "ami-074df373d6bafa625"
-  spot_price              = "0.0294"
+  spot_price              = "0.0031"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-0bcba096b73fa353d"]
   wait_for_fulfillment    = true
@@ -14,7 +14,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
 
 resource "aws_ec2_tag" "name-tag" {
   count                   = local.LENGTH
-  resource_id             = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  resource_id             = element(aws_spot_instance_request.cheap_worker.*.spot_intance_id, count.index)
   key                     = "Name"
   value                   = element(var.COMPONENTS,count.index)
 }
@@ -23,7 +23,7 @@ resource "aws_route53_record" "records" {
   count                   = local.LENGTH
   name                    = element(var.COMPONENTS,count.index )
   type                    = "A"
-  zone_id                 = "Z030489939510OB7G0257"
+  zone_id                 = "Z0918424VRBQYXPY7YV9"
   ttl                     = 300
   records                 = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
 }
