cb71b3710c9cd7a6328490cf987c263c850766a2
veeranki2014 <veeranki2014@gmail.com>
2021-08-18 19:35:07 +0530

    Demo on Datasources

--- 07-data-sources/data.tf
+++ 07-data-sources/data.tf
@@ -5,6 +5,20 @@ data "aws_ami" "example" {
   owners                = ["973714476881"]
 }
 
+data "aws_ec2_spot_price" "example" {
+  instance_type         = "t3.micro"
+  availability_zone     = "us-east-1a"
+
+  filter {
+    name   = "product-description"
+    values = ["Linux/UNIX"]
+  }
+}
+
+output "spot_price" {
+  value = data.aws_ec2_spot_price.example
+}
+
 output "amis" {
   value                 = data.aws_ami.example.id
 }
