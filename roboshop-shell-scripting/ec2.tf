6a11ab855d311c5b41f2f244b4e9b62f89d53bdf
veeranki2014 <veeranki2014@gmail.com>
2021-08-18 18:21:04 +0530

    Demo on Roboshop Project

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -14,7 +14,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
 
 resource "aws_ec2_tag" "name-tag" {
   count                   = local.LENGTH
-  resource_id             = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  resource_id             = element(aws_spot_instance_request.cheap_worker.*.spot_instance_i, count.index)
   key                     = "Name"
   value                   = element(var.COMPONENTS,count.index)
 }
