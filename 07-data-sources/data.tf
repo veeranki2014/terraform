acde497bb7208b8094665e829c3be7444e56307f
veeranki2014 <veeranki2014@gmail.com>
2021-08-18 19:31:05 +0530

    Demo on Datasources

--- 07-data-sources/data.tf
+++ 07-data-sources/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "example" {
   #executable_users = ["self"]
   most_recent           = true
-  name_regex            = "^centos*}"
+  name_regex            = "^Centos*"
   owners                = ["973714476881"]
 }
 
