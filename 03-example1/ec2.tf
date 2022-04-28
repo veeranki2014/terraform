fff87248b4b6071a7d297dd5a7e83725404e7959
veeranki2014 <veeranki2014@gmail.com>
2022-04-28 20:47:52 +0530

    EC2 Instance creation on AWS

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami                     = "ami-074df373d6bafa625"
+  ami                     = "ami-0bb6af715826253bf"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
 
