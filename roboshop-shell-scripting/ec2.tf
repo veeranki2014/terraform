213452a361c3692081cbad5dddf43c5d6ad75627
veeranki2014 <veeranki2014@gmail.com>
2022-05-07 22:35:20 +0530

    Roboshop app automation using terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,14 +1,14 @@
 # Request a spot instance at $0.03
 resource "aws_spot_instance_request" "cheap_worker" {
   count                   = local.LENGTH
-  ami                     = "ami-074df373d6bafa625"
+  ami                     = "ami-0bb6af715826253bf"
   spot_price              = "0.0031"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-0bcba096b73fa353d"]
   wait_for_fulfillment    = true
 
   tags                    = {
-    Name                  = "CheapWorker"
+    Name                  = element(var.COMPONENTS,count.index )
   }
 }
 
